cc1790680ba6062553a5d86adf8a5cbb072d3e03
singhmanish3007 <singhmanish3007@gmail.com>
2018-03-31 20:53:55 +0530

    My first commit

---
+++ src/main/java/com/cloudcompilerr/CloudCompilerrApplication.java
@@ -0,0 +1,13 @@
+package com.cloudcompilerr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCompilerrApplication {
+
+    public static void main(String args[]) {
+	SpringApplication.run(CloudCompilerrApplication.class, args);
+    }
+
+}
